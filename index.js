4bdadba0f747624da99ea1bb3016fb982511495f
SAURABH KUMAR <saurabh.soni119@gmail.com>
2023-08-31 17:38:59 +0530

    seach api with nane ans username

--- index.js
+++ index.js
@@ -5,28 +5,18 @@ const AdminDetails = require('./admin');
 const app = express();
 app.use(express.json());
 
-app.post('/create', async (req, res) => {
-    let data = new AdminDetails(req.body);
-    let result = await data.save();
-    console.log(req.body);
-    res.send(result);
+
+app.get("/search/:key", async (req, res) => {
+  let data = await AdminDetails.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { username: { $regex: req.params.key } },
+    ],
+  });
+  res.send(data);
 });
 
 
-app.get('/list', async (req, res) => {
-    let data =await AdminDetails.find();
-    res.send(data);
-});
-
-app.delete('/delete/:_id', async (req, res) => {
-    let data = await AdminDetails.deleteOne(req.params);
-    res.send(data);
-});
-
-app.put('/update/:_id', async (req, res) =>{
-    let data = await AdminDetails.updateOne(req.params, {$set:req.body});
-    res.send(data);
-})
 
 
 app.listen(5000);
