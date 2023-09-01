7fd25a277afe4fda8dc724783d62fd7fc1d5b46e
SAURABH KUMAR <saurabh.soni119@gmail.com>
2023-09-01 22:23:12 +0530

    complete crud operation using mysql

--- config.js
+++ config.js
@@ -1,2 +1,21 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/admin');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'employee_database',
+    port: 3306
+});
+
+connection.connect((error) =>{
+    if(error){
+        console.log("error in database connection");
+    } else {
+        console.log('mysql connection successfully');
+    }
+});
+
+
+module.exports = connection;
+
--- index.js
+++ index.js
@@ -1,21 +1,67 @@
-const mysql = require('mysql2');
+const express = require("express");
+const connection = require("./config");
+const app = express();
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'employee_database',
-    port: 3306
-});
+app.use(express.json());
 
-connection.connect((error) =>{
-    if(error){
-        console.log("error", error);
+app.get("/", (req, res) => {
+  connection.query("select * from employee_info", (error, result) => {
+    if (error) {
+      res.send("error");
     } else {
-        console.log("connected");
+      res.send(result);
     }
+  });
 });
 
-connection.query("select * from employee_info", (error, result)=>{
-  console.log('result', result)
-})
+app.post("/create-employee", (req, res) => {
+  const data = req.body;
+  connection.query(
+    "INSERT INTO employee_info SET ?",
+    data,
+    (error, result, fields) => {
+      if (error) throw error;
+      res.send(result);
+    }
+  );
+});
+
+app.put("/:emp_id", (req, res) => {
+  const data = [
+    req.body.first_name,
+    req.body.last_name,
+    req.body.phone,
+    req.body.email,
+    req.body.reporting_manager,
+    req.params.emp_id,
+  ];
+
+  connection.query(
+    "UPDATE employee_info SET first_name = ?, last_name = ?, phone = ?, email = ?, reporting_manager = ? WHERE emp_id = ?",
+    data,
+    (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send("Error updating employee");
+      } else {
+        res.send(results);
+      }
+    }
+  );
+});
+
+app.delete("/:emp_id", (req, res) => {
+  connection.query(
+    "DELETE FROM employee_info Where emp_id = " + req.params.emp_id,
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send("Error deleting employee");
+      } else {
+        res.send(results);
+      }
+    }
+  );
+});
+
+app.listen(5000);
